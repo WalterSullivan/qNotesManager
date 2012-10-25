dbade061e2eed99e9118634ba5393c1ab191bd20
WalterSullivan <waltersullivan.11121@gmail.com>
2012-10-25 15:25:30 +0400

    Debug messages are not displayed in Release configuration now

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,9 @@ void myMessageOutput(QtMsgType type, const char *msg) {
 	}
 	switch (type) {
 	case QtDebugMsg:
+#ifdef DEBUG
 		fprintf(stderr, "Debug: %s\n", msg);
+#endif
 		break;
 	case QtWarningMsg:
 		fprintf(stderr, "Warning: %s\n", msg);
