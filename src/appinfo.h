6492ce53aad5b6e7ef64d3327085e04a5a2fb055
WalterSullivan <waltersullivan.11121@gmail.com>
2014-02-14 10:37:58 +0400

    Added ALPHA and RC macros, changed version to 0.9.5-a

--- src/appinfo.h
+++ src/appinfo.h
@@ -6,12 +6,21 @@
 
 #define V_MAJOR 0
 #define V_MINOR 9
-#define V_BUILD 4
+#define V_BUILD 5
 #define V_REVISION 0
+#define V_ALPHA 1
+#define V_RC 0
 
 #define V_VERSION  V_MAJOR.V_MINOR.V_BUILD.V_REVISION
 #define V_SVERSION  V_MAJOR.V_MINOR.V_BUILD
+
+#if V_ALPHA>0
+#define V_SVERSION_STR quote(V_SVERSION)"-a"
+#elif V_RC>0
+#define V_SVERSION_STR quote(V_SVERSION)"-rc"quote(V_RC)
+#else
 #define V_SVERSION_STR quote(V_SVERSION)
+#endif
 
 #define VER_FILEVERSION             V_MAJOR,V_MINOR,V_BUILD,V_REVISION
 #define VER_FILEVERSION_STR         quote(V_VERSION)
