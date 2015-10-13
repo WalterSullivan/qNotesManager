26e3bde0162a3e1ac5a2ee3326de87c4563f04d3
WalterSullivan <waltersullivan.11121@gmail.com>
2015-10-13 09:31:13 +0300

    appinfo reworked

--- src/appinfo.h
+++ src/appinfo.h
@@ -1,32 +1,29 @@
 ﻿#ifndef APPINFO_H
 #define APPINFO_H
 
-#define stringify(v1) #v1
-#define quote(v1)  stringify(v1)
+#define stringify2(v1) #v1
+#define stringify(v1)  stringify2(v1)
 
-#define V_MAJOR 0
-#define V_MINOR 9
-#define V_BUILD 6
-#define V_REVISION 0
-#define V_ALPHA 0
-#define V_RC 0
+#define V_MAJOR		0
+#define V_MINOR		9
+#define V_BUILD		7
+#define V_REVISION	0
+#define V_ALPHA		1
 
 #define V_VERSION  V_MAJOR.V_MINOR.V_BUILD.V_REVISION
 #define V_SVERSION  V_MAJOR.V_MINOR.V_BUILD
 
 #if V_ALPHA>0
-#define V_SVERSION_STR quote(V_SVERSION)"-a"
-#elif V_RC>0
-#define V_SVERSION_STR quote(V_SVERSION)"-rc"quote(V_RC)
+#define V_SVERSION_STR stringify(V_SVERSION) "-a"
 #else
-#define V_SVERSION_STR quote(V_SVERSION)
+#define V_SVERSION_STR stringify(V_SVERSION)
 #endif
 
 #define VER_FILEVERSION             V_MAJOR,V_MINOR,V_BUILD,V_REVISION
-#define VER_FILEVERSION_STR         quote(V_VERSION)
+#define VER_FILEVERSION_STR         V_SVERSION_STR
 
 #define VER_PRODUCTVERSION          V_MAJOR,V_MINOR,V_BUILD,V_REVISION
-#define VER_PRODUCTVERSION_STR      quote(V_SVERSION)
+#define VER_PRODUCTVERSION_STR      V_SVERSION_STR
 
 #define VER_COMPANYNAME_STR         "Yury Khamenkov"
 #define VER_FILEDESCRIPTION_STR     "qNotesManager"
