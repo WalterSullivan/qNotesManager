6cdca8397a8c1efcb8541a08e1e243aa7aae3ded
WalterSullivan <waltersullivan.11121@gmail.com>
2016-02-18 13:35:56 +0300

    Added missing switch branches.

--- src/main.cpp
+++ src/main.cpp
@@ -115,6 +115,11 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
 	case QtFatalMsg:
 		fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
 		abort();
+	case QtInfoMsg:
+		fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		break;
+	default:
+		break;
 	}
 }
 #else
